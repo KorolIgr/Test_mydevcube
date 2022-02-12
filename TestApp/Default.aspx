0579c6d5d0de5a71474e19a484ff2b8f59b9e6a1
Korol Igor <korol.igor.alexandrovi4@gmail.com>
2022-02-12 18:47:16 +0600

    First version

---
+++ TestApp/Default.aspx
@@ -0,0 +1,19 @@
+﻿<%@ Page Language="vb" AutoEventWireup="false" CodeBehind="Default.aspx.vb" Inherits="TestApp.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>Test app</title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:Button ID="Button1" runat="server" Text="Update" />
+        </div>
+        <div>
+            <asp:PlaceHolder ID="PlaceHolder1" runat="server"></asp:PlaceHolder>
+        </div>
+    </form>
+</body>
+</html>
---
+++ TestApp/Default.aspx.vb
@@ -0,0 +1,75 @@
+﻿Imports System.Data.SqlClient
+
+Public Class WebForm1
+    Inherits System.Web.UI.Page
+
+    Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
+
+    End Sub
+
+    Protected Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+
+        Dim connetionString As String
+        Dim connection As SqlConnection
+        Dim command As SqlCommand
+        Dim adapter As New SqlDataAdapter
+        Dim ds As New DataSet
+        Dim i As Integer
+        Dim sql As String
+
+        connetionString = "Data Source=IGOR_PC\SQLEXPRESS;Initial Catalog=TT;Integrated Security=True"
+        sql = "SELECT 
+	                T2.Name, 
+	                T2.Address, 
+	                T1.Number, 
+	                T1.Date, 
+	                T1.Description, 
+	                T1.Amount 
+                FROM 
+	                [dbo].[Orders] as T1
+                LEFT JOIN 
+	                [dbo].[Customers] as T2
+                ON 
+	                T1.Customer_ID = T2.Id"
+
+        connection = New SqlConnection(connetionString)
+
+        Try
+            connection.Open()
+            command = New SqlCommand(sql, connection)
+            adapter.SelectCommand = command
+            adapter.Fill(ds)
+            adapter.Dispose()
+            command.Dispose()
+            connection.Close()
+
+
+            Dim Table As New StringBuilder
+            Table.Append("<table border=1><tr>")
+            Table.Append("<th>Customer Name</th><th>Customer Address</th><th>Order number</th><th>Order date</th><th>Order Description</th><th>Order amount</th>")
+
+            For i = 0 To ds.Tables(0).Rows.Count - 1
+
+                Table.Append("<tr>")
+                Table.Append("<td>" & ds.Tables(0).Rows(i).Item(0) & "</td>")
+                Table.Append("<td>" & ds.Tables(0).Rows(i).Item(1) & "</td>")
+                Table.Append("<td>" & ds.Tables(0).Rows(i).Item(2) & "</td>")
+                Table.Append("<td>" & ds.Tables(0).Rows(i).Item(3) & "</td>")
+                Table.Append("<td>" & ds.Tables(0).Rows(i).Item(4) & "</td>")
+                Table.Append("<td>" & ds.Tables(0).Rows(i).Item(5) & "</td>")
+
+            Next
+
+            Table.Append("</table>")
+
+
+            PlaceHolder1.Controls.Add(New Literal With {.Text = Table.ToString})
+
+        Catch ex As Exception
+            MsgBox("Can not open connection ! ")
+        End Try
+
+
+
+    End Sub
+End Class
